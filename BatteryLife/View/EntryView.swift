58eb0c51803d5512c7a145e5eecbcba1edd962cc
Yukun Xie <yxie@lcs.on.ca>
2024-05-29 10:01:43 -0400

    Created entry interface

---
+++ BatteryLife/View/EntryView.swift
@@ -0,0 +1,29 @@
+//
+//  EntryView.swift
+//  BatteryLife
+//
+//  Created by Yukun Xie on 2024/5/29.
+//
+
+import SwiftUI
+
+struct EntryView: View {
+    var body: some View {
+        ZStack {
+            Image(systemName: "circle")
+                .font(.system(size: 280))
+                .foregroundStyle(.green)
+            Image(systemName: "bolt.fill")
+                .foregroundStyle(.white)
+                .font(.system(size: 150))
+        }
+        .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.black)
+    }
+    
+}
+
+#Preview {
+    EntryView()
+}
