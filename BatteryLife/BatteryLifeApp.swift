5e41ab942fc8c86fc03cd26850305890397a25bb
Yukun Xie <yxie@lcs.on.ca>
2024-05-29 09:47:46 -0400

    Initial Commit

---
+++ BatteryLife/BatteryLifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BatteryLifeApp.swift
+//  BatteryLife
+//
+//  Created by Yukun Xie on 2024/5/29.
+//
+
+import SwiftUI
+
+@main
+struct BatteryLifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BatteryLife/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BatteryLife
+//
+//  Created by Yukun Xie on 2024/5/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
